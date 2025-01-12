4fa51e91f165f4dbf0bd914df2e68a434c1ae317
Pavan Prakash K <pavancodes05@gmail.com>
2025-01-12 13:25:36 +0530

    Part-1 Completed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const apiRoutes = require('./routes/index.js')
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { InfoController } = require('../../controllers') 
+const AirplaneRoutes = require('./airplane-routes')
 
+router.use("/airplanes", AirplaneRoutes);
 router.get("/info", InfoController.info)
+
 
 module.exports = router;  
